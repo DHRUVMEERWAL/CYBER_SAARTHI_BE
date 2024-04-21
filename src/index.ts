7e1e86e2b116aec3e8435f759b5b1ea2875f2cab
Dhruv <dhruv@MacBook-Pro.local>
2024-04-21 13:22:52 +0530

    origin changed

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Hono<{
 }>()
 
 app.use('*', cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://main--regal-gelato-a3822f.netlify.app',
   allowHeaders: ['Content-Type', 'Authorization'],
   allowMethods: ['POST', 'GET', 'OPTIONS'],
   exposeHeaders: ['Content-Length'],
